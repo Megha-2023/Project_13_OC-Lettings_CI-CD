1aea50b56e434a903da85189a91af62528bd0288
Megha-2023 <mh.panchal2019@gmail.com>
2025-05-28 17:36:09 +0200

    Written tests for lettings app

--- lettings/tests/test_letting.py
+++ lettings/tests/test_letting.py
@@ -3,15 +3,29 @@ from django.urls import reverse
 from lettings.models import Letting, Address
 
 
-@pytest.fixture
+@pytest.fixture(name="sample")  # to remove redefined-outer-name pylint error
 def sample_lettings():
-    address = Address.objects.create(number=26, street="Rue", city="Ville", state="Provence", zip_code=1234, country_iso_code="+33")
+    address = Address.objects.create(
+        number=26,
+        street="Rue",
+        city="Ville",
+        state="Provence",
+        zip_code=1234,
+        country_iso_code="+33"
+    )
     letting = Letting.objects.create(title="Test Letting", address=address)
     return letting
 
 
 @pytest.mark.django_db
-def test_lettings_index(client, sample_lettings):  #, create_lettings):
+def test_lettings_index(client, sample):
     response = client.get(reverse('lettings:lettings_index'))
     assert response.status_code == 200
-    assert sample_lettings.title in str(response.content)  # .decode("utf-8")
+    assert sample.title in str(response.content)
+
+
+@pytest.mark.django_db
+def test_letting_with_id(client, sample):
+    response = client.get(reverse('lettings:letting', kwargs={'letting_id': sample.id}))
+    assert response.status_code == 200
+    assert sample.title in str(response.content)
