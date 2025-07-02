577fb10b107a00f86554fd5878398a40504080b6
Megha Panchal <mh.panchal2019@gmail.com>
2025-07-02 17:30:40 +0200

    Added index page test and pytest to pipeline

---
+++ oc_lettings_site/tests/test_site_home.py
@@ -0,0 +1,8 @@
+import pytest
+from django.urls import reverse
+
+@pytest.mark.django_db
+def test_site_index_page(client):
+    response = client.get(reverse('index'))
+    assert response.status_code == 200
+    assert "Holiday Homes" in str(response.content)
