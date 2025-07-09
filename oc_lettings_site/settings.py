ac2eae6e14040eeb8c066dc9edb5ea69ab637359
Megha Panchal <mh.panchal2019@gmail.com>
2025-07-09 11:52:41 +0200

    Updated autofield settings.

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -19,6 +19,8 @@ SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 ALLOWED_HOSTS = [os.environ.get("DJANGO_ALLOWED_HOSTS"), "localhost",  "127.0.0.1"]
 
 # Application definition
