71898306d16a32030c835ae7650e3f9343a139ed
Megha Panchal <mh.panchal2019@gmail.com>
2025-07-09 10:35:06 +0200

    Changed versions of all packages for compatibility.

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -5,7 +5,7 @@ from .views import index, trigger_error
 
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='index'),
     path('lettings/', include('lettings.urls'), name='lettings_index'),
     path('profiles/', include('profiles.urls'), name='profiles_index'),
     path('admin/', admin.site.urls),
